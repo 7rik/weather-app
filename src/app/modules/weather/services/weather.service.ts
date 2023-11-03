8160cd632613e0fa9ee466a1e2ab0f098f6a5669
7rik <bezerra.erik2003@gmail.com>
2023-11-03 17:23:01 -0300

    criados módulos de componentes e services

---
+++ src/app/modules/weather/services/weather.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  private apiKey = 'ab3af4b0196a9c6d645f9866b49041c5';
+
+  constructor(private http: HttpClient) { }
+
+  public getWeatherDatas(cityName: string): Observable<any>{
+    return this.http.get(
+      `htttps://api.openweathermap.org/data/2.5/weather?
+        q=${cityName}&units=metric&mode=json&appid${this.apiKey}`, {})
+  }
+}
